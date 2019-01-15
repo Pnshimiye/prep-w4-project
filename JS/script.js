1d4cefce4c3a14d89e24369282b8c6c80df442af
Pnshimiye <pnshimiye@gmail.com>
2019-01-15 09:16:27 +0200

    add style

--- JS/script.js
+++ JS/script.js
@@ -42,3 +42,72 @@ Turn.prototype.endTurn = function(player1, player2) {
       $("#player1").toggleClass("active");
   };
 };
+$(document).ready(function() {
+  var player1 = new Player("Player 1");
+  var player2 = new Player("Player 2");
+
+  //Start current turn with player1, will be switched to player2 in endTurn
+  var currentTurn = new Turn(player1);
+
+  var total = currentTurn.total;
+
+  //jquery animations
+  $("#player1").hide().show("slow");
+  $("#player2").hide().show("slow");
+  $("h1").hide().show("slow");
+  $("p").hide().show("slow");
+
+  //Prints initial Turn Total of 0
+  $("#roll-total").text(total);
+
+  //Prints initial player scores of 0
+  $('#player1-score').text(player1.score);
+  $('#player2-score').text(player2.score);
+
+  //Prints current Player
+  $('#current_player').text(currentTurn.player.userName);
+
+  //This code runs when you click the Roll button
+  $("form#roll").submit(function(event) {
+    event.preventDefault();
+
+    //Creates a dice roll number
+    var result = currentTurn.diceRoller(player1, player2);
+
+    //Prints the roll number to the page
+    $('#roll').text(result);
+
+    //Prints the roll total to the page
+    $('#roll-total').text(currentTurn.total);
+
+    //Determines the winner and prints player score to page
+    if ((currentTurn.total + currentTurn.player.score) >= 100) {
+      if (currentTurn.player == player1) {
+          $('#player1-score').text(currentTurn.total + currentTurn.player.score);
+          alert("You are the winner!");
+      } else if (currentTurn.player == player2) {
+          $('#player2-score').text(currentTurn.total + currentTurn.player.score)
+          alert("You are the winner!");
+      };
+    };
+  });
+
+  //This code runs when you click the End Turn button
+  $("form#end-turn").submit(function(event) {
+    event.preventDefault();
+
+    currentTurn.endTurn(player1, player2);
+
+    //Prints current Player
+    $('#current_player').text(currentTurn.player.userName);
+
+    //Prints players scores
+    $('#player1-score').text(player1.score);
+    $('#player2-score').text(player2.score);
+
+    //Prints the cleared Current Roll and Turn Total on page
+    $('#roll').text(currentTurn.randNumber);
+    $('#roll-total').text(currentTurn.total);
+  });
+});
+
